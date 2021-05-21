badd404b6452d5939db45fb8d52ee6b472bb1199
Anna-Johnson <44846295+Anna-Johnson@users.noreply.github.com>
2021-05-21 12:55:19 -0400

    Update BMPs_Cleanup.R
    further updates to ER cleaning scripts

--- PGLMT_scripts/BMPs_Cleanup.R
+++ PGLMT_scripts/BMPs_Cleanup.R
@@ -29,7 +29,19 @@ require(stringr)
 #orient to correct working directory, just to be sure
 here::i_am("PGLMT_scripts/BMPs_Cleanup.R")
 
-#Clean up ER Matrix
+#################################################################################
+################################################################################
+
+#########################
+#1. Clean up ER Matrix ##
+#########################
+
+################################################################################
+################################################################################
+
+##########################################################
+#1a. extract and format sp x code sheets for each agency #
+##########################################################
 
 #turn ER Matrix into object
 ERmat <- "C:/Users/AJohnson/Documents/PGLMT/PGLMT_scripts/MatrixRules_FEB2020.xlsx"
@@ -84,7 +96,109 @@ k <- str_extract_all(ER_GuildxAction$Guild, "\\([^()]+\\)") [[1]]
 k <- substring(k, 2, nchar(k)-1) #remove parentheses
 
 ER_GuildxAction$Guide_ID <- k
+#the problems are for "A" and "S', the NLEB ones, "Within.Bog.Turtle,.but.outside.known.watershed", and geology--those have to be fixed by hand either prior to reading in, or post-hoc
 
+#export finished file for future reference/so you don't have to rebuild it until it gets updated
+#write.csv(ER_GuildxAction, file="ER_GuildxAction_long.csv")
+#read back in file now that it has been slighly hand cleaned (fixed the guild codes that didn't work with parentheses)
+
+
+ER_GuildxAction <- read.csv(file="ER_GuildxAction_long.csv")
+
+#also some of the matrix cells have a comma separating multiple responses, need to address
+ER_GuildxAction2 <- ER_GuildxAction %>% separate(Response, c("R1","R2"), sep = "([,])")
+
+#reformat back into long, and then remove missing data rows
+ER_GuildxAction2 <- gather(ER_GuildxAction2, temp, Response, R1:R2)
+ER_GuildxAction2 <- ER_GuildxAction2[complete.cases(ER_GuildxAction2),] #remove rows w/ missing data, if they are in there
+
+#remove unnecessary columns
+drops <- c("X","temp")
+ER_GuildxAction2 <- ER_GuildxAction2[ , !(names(ER_GuildxAction2) %in% drops)]
+names(ER_GuildxAction2)[1] <- "ER_Code" #rename ER Code column
+#write further cleaned .csv file to store
+write.csv(ER_GuildxAction2, file="ER_GuildxAction_formatted.csv")
+
+#############################################################
+#1b. extract and reformat the further questions spreadsheet #
+#############################################################
+
+#reformat the further questions spreadsheet
+ERmat_sheets
+n <- 8 # enter its location in the list (for Avoidance Measures)
+#this one had to have some manual edits made to it, because some of the questions had follow-up questions, but they weren't set up in a way that allowed easy automatic extraction.
+#Created two new columns--one which attributed each follow up level to the original question, and one which indicated whether the question had a set of follow-up questions
+
+ER_mat_table <- read.xlsx(xlsxFile=ERmat, sheet=ERmat_sheets[n], skipEmptyRows=FALSE, rowNames=FALSE)
+
+names(ER_mat_table)
+
+#select out just the main question text (not the follow-up sub-questions)
+
+ER_Q_main <- subset(ER_mat_table, ER_mat_table$Follow_up %in% c("Y", "N"))
+
+ER_Q_sub <- subset(ER_mat_table, ER_mat_table$Follow_up =="sub")
+
+#convert from wide to long
+ER_Q_main <- gather(ER_Q_main, Q_Response, BMP, Yes:Unknown, factor_key=TRUE)
+ER_Q_main <- ER_Q_main[complete.cases(ER_Q_main),] #remove rows w/ missing data as a result of missing Q cases
+
+ER_Q_sub <- gather(ER_Q_sub, Q_Response, BMP, Yes:Unknown, factor_key=TRUE)
+ER_Q_sub <- ER_Q_sub[complete.cases(ER_Q_sub),] 
+
+write.csv(ER_Q_main, file="ER_Questions_main.csv") #write out the result
+write.csv(ER_Q_sub, file="ER_Questions_sub.csv") #write out the result
+
+################################################
+#1c. extract and reformat the BMP spreadsheets #
+################################################
+
+#create long-formatted spreadsheets with the Conservation Measures, Avoidance Measures, and responses to questions
+
+#get a list of the sheets in the ER matrix file
+ERmat_sheets
+
+#look at the output and choose which excel sheet you want to load
+
+n <- 9 # enter its location in the list (for Avoidance Measures)
+
+ER_mat_table <- read.xlsx(xlsxFile=ERmat, sheet=ERmat_sheets[n], skipEmptyRows=FALSE, rowNames=FALSE)
+
+head(ER_mat_table)
+
+AvoidanceMeasures <- ER_mat_table
+names(AvoidanceMeasures) <- c("ER_BMP_Code","TargetGuilds","Text") #rename columns
+
+n <- 10 # enter its location in the list (for Conservation Measures)
+
+ER_mat_table <- read.xlsx(xlsxFile=ERmat, sheet=ERmat_sheets[n], skipEmptyRows=FALSE, rowNames=FALSE)
+ConservationMeasures <- ER_mat_table
+names(ConservationMeasures) <- c("ER_BMP_Code","TargetGuilds","Text")
+
+n <- 11 # enter its location in the list (for Conservation Measures)
+
+ER_mat_table <- read.xlsx(xlsxFile=ERmat, sheet=ERmat_sheets[n], skipEmptyRows=FALSE, rowNames=FALSE)
+InformationRequest <- ER_mat_table
+names(InformationRequest) <- c("ER_BMP_Code","TargetGuilds","Text")
+
+#glue them all together--first create list of df so that the name of each individual BMP table can be incorporated into big table as a factor
+BMPs <- list(AvoidanceMeasures, ConservationMeasures, InformationRequest)
+names(BMPs) <-c("AvoidanceMeasures", "ConservationMeasures", "InformationRequest")
+
+ER_BMPs_bycode <- bind_rows(BMPs, .id="BMP_type")
+names(ER_BMPs_bycode)
+
+#export result for future ref
+write.csv(ER_BMPs_bycode, file="ER_BMPs_bycode.csv")
+
+############################################################################################
+############################################################################################
+
+########################################
+## 2. Clean up PGC-ER crosswalk files ##
+########################################
+
+###########################################################################################
 
 #read in PGC-ER activity crosswalk files
 ER_PGC_cw <- read.csv("ER_PGC_Crosswalk.csv")
